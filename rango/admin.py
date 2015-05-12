1bf0dd81d4cd924d84003308ec7125aba4fbd277
Liang <greatliang@gmail.com>
2015-05-12 07:39:32 +0300

    6.9. Exercises: add admin interface and category views and likes

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
+
+class PageAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['title']}),
+        ('Category', {'fields': ['category']}),
+        ('URL', {'fields': ['url']}),
+    ]
+    list_display = ('title', 'category', 'url')
+
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
