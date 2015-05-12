f97e664fe38256491708ddb3ae01fbfabd3f82fd
Liang <greatliang@gmail.com>
2015-05-12 08:22:27 +0300

    7.3.2. Update Category Table with Slug Field

--- rango/admin.py
+++ rango/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
 class PageAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['title']}),
@@ -10,5 +14,5 @@ class PageAdmin(admin.ModelAdmin):
     ]
     list_display = ('title', 'category', 'url')
 
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
