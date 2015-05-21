6489a6c520d6215958f8bc2de64bca3109f36c08
Liang <greatliang@gmail.com>
2015-05-21 08:34:31 +0300

    8.3.1. Creating an Add Pages View, Template and URL Mapping

--- rango/forms.py
+++ rango/forms.py
@@ -29,7 +29,7 @@ class PageForm(forms.ModelForm):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
-        if url and not url.stratswith('http://'):
+        if url and not url.startswith('http://'):
             url = 'http://' + url
             cleaned_data['url'] = url
         return cleaned_data
