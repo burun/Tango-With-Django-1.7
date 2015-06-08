3f78f7c39b3365d7d7b3b998a4911773912a67c0
Liang <greatliang@gmail.com>
2015-06-08 07:20:28 +0300

    18.2. Example Popup Box on Click

--- static/js/rango-jquery.js
+++ static/js/rango-jquery.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
-        // JQuery code to be added in here.
+    $("#about-btn").click( function(event) {
+        alert("You clicked the button using JQuery!");
+    });
 
 });
