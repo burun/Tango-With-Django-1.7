d3456737ab613b238cf552cc710220959af74b61
Liang <greatliang@gmail.com>
2015-06-10 08:53:39 +0300

    20.2. Testing the models in Rango

--- rango/models.py
+++ rango/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
-    views = models.IntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
     likes = models.IntegerField(default=0)
     slug = models.SlugField(unique=True)
 
