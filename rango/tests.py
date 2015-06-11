b457b2e614c2380a959be82398927e794bf48a72
Liang <greatliang@gmail.com>
2015-06-11 07:53:16 +0300

    20.3. Testing Views

--- rango/tests.py
+++ rango/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from rango.models import Category
+from django.core.urlresolvers import reverse
 
 
 class CategoryMethodTests(TestCase):
@@ -22,3 +23,15 @@ class CategoryMethodTests(TestCase):
         cat = Category(name='Random Category String')
         cat.save()
         self.assertEqual(cat.slug, 'random-category-string')
+
+
+class IndexViewTests(TestCase):
+
+    def test_index_view_with_no_categories(self):
+        '''
+        If no questions exist, an appropriate message should be displayed.
+        '''
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "There are no categories present.")
+        self.assertQuerysetEqual(response.context['categories'], [])
